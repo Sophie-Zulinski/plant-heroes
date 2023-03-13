24adfa4675354f21f0fe95ca36e85211950f78eb
Sophie-Zulinski <sophie.zulinski@gmx.at>
2023-03-13 14:00:00 +0100

    Start matching page

---
+++ app/matches/[username]/page.js
@@ -0,0 +1,21 @@
+import { notFound } from 'next/navigation';
+import { getUserByUsername, getUsers } from '../../../database/users';
+import Dashboardmatches from './Dashboardmatches';
+
+export default async function UserMatches({ params }) {
+  const user = await getUserByUsername(params.username);
+  const users = await getUsers();
+  if (!user) {
+    notFound();
+  }
+
+  return (
+    <main className="min-h-screen">
+      <div className="flex justify-center items-center ">
+        <div className="flex flex-col p-20 gap-4 bg-white rounded-md mt-10 justify-center items-center">
+          <Dashboardmatches user={user} users={users} />
+        </div>
+      </div>
+    </main>
+  );
+}
